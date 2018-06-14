b8568281f97f9d4963c7f1a3242cf49c76aba64e
Samuel locatelli <mukatk@gmail.com>
2018-06-13 22:08:16 -0300

    Colocando cores e comentários

--- mandelbrot.c
+++ mandelbrot.c
@@ -3,22 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 #include <math.h>
 #include "workstash.h"
 #define NUM_THREADS 16
-#define WINDOW_SIZE 860
+#define WINDOW_SIZE 640
 #define NUM_BLOCKS_PER_LINE 4
 
+// Uma thread consumidora
 pthread_t thread_cons;
+
+// 16 threads para calcular
 pthread_t thread_prod[NUM_THREADS];
 
+//Mutex para o buffer
 pthread_mutex_t buffer_mutex;
+
+//Variável de condição
 pthread_cond_t vc;
 
+//Declaração da thread consumidora
 void *consumer(void *arg);
 
+//Declaração da thread produtora
 void *producer(void *arg);
 
+//Struct para definir a região de atuação da thread produtora
 typedef struct {
 	int initial_x;
 	int final_x;
@@ -26,8 +36,10 @@ typedef struct {
 	int final_y;
 } region_param, *region_param_arg;
 
+//array com as 16 regiões para serem calculadas
 region_param regions[NUM_THREADS];
 
+//Constantes para o cálculo da fractal
 const double CxMin=-2.5;
 const double CxMax=1.5;
 const double CyMin=-2.0;
@@ -35,11 +47,20 @@ const double CyMax=2.0;
 
 int main(int argc, char **argv) {
 	int i;	
+	//inicializa o mutex
 	pthread_mutex_init(&buffer_mutex, NULL);
+
+	//Seta o tamanho da janela
 	int window_width = WINDOW_SIZE, window_height = WINDOW_SIZE;
-	int block_size = WINDOW_SIZE / (NUM_THREADS / NUM_BLOCKS_PER_LINE);
+
+	// Calcula o tamanho do quadrado da região
+	int block_size = WINDOW_SIZE / NUM_BLOCKS_PER_LINE;
 	
+	//x, y de descolamento
 	int offset_x, offset_y = 0;
+
+	//Cria thread do consumidor
+	pthread_create(&thread_cons, NULL, consumer, NULL);
 
 	for(i = 0; i < NUM_THREADS; i++) {
 		regions[i].initial_x = offset_x;
@@ -55,24 +76,28 @@ int main(int argc, char **argv) {
 			offset_y += block_size;
 		}
 
+		//Cria uma thread produtora
 		pthread_create(&(thread_prod[i]), NULL, producer, &(regions[i]));
 	}
 
-	pthread_create(&thread_cons, NULL, consumer, NULL);
-	
+	//espera finalização das threads produtoras
 	for(i = 0 ; i < NUM_THREADS; i++) {
 		pthread_join(thread_prod[i], NULL);
 	}
 	
+	//Espera finalização da thread consumidora
 	pthread_join(thread_cons, NULL);
 
 	return 0;
 }
 
 void *producer(void *arg) {
+	//Recebe a região via parametro da thread
 	region_param_arg region = (region_param_arg)arg;
-	int iYmax = WINDOW_SIZE;
-	int iXmax = WINDOW_SIZE;
+
+	int size = WINDOW_SIZE;
+	int iYmax = size;
+	int iXmax = size;
 	double Cx,Cy;
 	double PixelWidth=(CxMax - CxMin)/iXmax;
 	double PixelHeight=(CyMax - CyMin)/iYmax;
@@ -80,18 +105,19 @@ void *producer(void *arg) {
 	double Zx, Zy;
 	double Zx2, Zy2;
 	int Iteration;
-	const int IterationMax = 200;
+	const int IterationMax = 256;
 	const double EscapeRadius = 2;
 	double ER2 = EscapeRadius * EscapeRadius;
 	int iX,iY;
 	int color;
 
-	for(iY = region->initial_y; iY < iYmax;iY++)
+	//Calcula a fractal para cada região
+	for(iY = region->initial_y; iY < region->final_y;iY++)
 	{
 		Cy = CyMin + iY * PixelHeight;
 		if (fabs(Cy) < PixelHeight/2)
 			Cy=0.0;
-		for (iX = region->initial_x;iX < iXmax; iX++)
+		for (iX = region->initial_x;iX < region->final_x; iX++)
 		{         
 				Cx= CxMin + iX * PixelWidth;
 				
@@ -115,28 +141,30 @@ void *producer(void *arg) {
 				else 
 				{
 					color = Iteration;
-				};
+				}
 
-				pthread_mutex_lock(&buffer_mutex);
+				//Insere o pixel a ser printado na tela
+				pthread_mutex_lock(&buffer_mutex); //Lock do mutex
 				struct work_param work;
 				work.x = iX;
 				work.y = iY;
 				work.color = color;
-				push_work(work);
-				pthread_cond_signal(&vc);
-				pthread_mutex_unlock(&buffer_mutex);
+				push_work(work); //Coloca o resultado em uma pilha
+				pthread_cond_signal(&vc); //Emite um sinal para alertar a thread consumidora que tem algo para consumir
+				pthread_mutex_unlock(&buffer_mutex); //unlock do mutex
 		}
 	}
 }
 
 void *consumer(void *arg) {
+	// Inicializa uma janela do XLib
+
 	Display *display;
     Window window;
     XEvent event;
     int count = 0;
     int s;
  
-    /* open connection with the server */
     display = XOpenDisplay(NULL);
     if (display == NULL)
     {
@@ -146,45 +174,51 @@ void *consumer(void *arg) {
  
     s = DefaultScreen(display);
  
-    /* create window */
     window = XCreateSimpleWindow(display, RootWindow(display, s), 10, 10, WINDOW_SIZE, WINDOW_SIZE, 1,
                            BlackPixel(display, s), WhitePixel(display, s));
  
-    /* select kind of events we are interested in */
     XSelectInput(display, window, ExposureMask | KeyPressMask);
- 
-    /* map (show) the window */
     XMapWindow(display, window);
 
-	XColor xcolour;
+	XColor red, blue, orange;
 	Colormap cmap = DefaultColormap(display, s);
+	red.red = 32000; red.green = 0; red.blue = 0;
+	red.flags = DoRed | DoGreen | DoBlue;
 
-	// I guess XParseColor will work here
-	xcolour.red = 32000; xcolour.green = 0; xcolour.blue = 0;
-	xcolour.flags = DoRed | DoGreen | DoBlue;
-	XAllocColor(display, cmap, &xcolour);
+	blue.red = 0; blue.green = 0; blue.blue = 32000;
+	blue.flags = DoRed | DoGreen | DoBlue;
+
+	orange.red = 64000; orange.green = 5000; orange.blue = 2500;
+	orange.flags = DoRed | DoGreen | DoBlue;
+
+	XAllocColor(display, cmap, &red);
+	XAllocColor(display, cmap, &blue);
+	XAllocColor(display, cmap, &orange);
 
 	while (1) {
-		pthread_mutex_lock(&buffer_mutex);
-		if (top == NULL) {
-			pthread_cond_wait(&vc, &buffer_mutex);
+		pthread_mutex_lock(&buffer_mutex); //Lock do mutex
+		if (top == NULL) { // Se o topo do da pilha for nulo, ou seja, nenhum pixel para printar
+			pthread_cond_wait(&vc, &buffer_mutex); //Emite um wait
 		}
-		struct work_param result = pop_work();
+		struct work_param result = pop_work(); //Retira o pixel do topo da pilha
         
-		if (result.color != 255) {
-			XSetForeground(display, DefaultGC(display, s), xcolour.pixel * result.color);	
+		//seta a cor
+		if (result.color >= 10) {
+			XSetForeground(display, DefaultGC(display, s), red.pixel);
+		} else if (result.color < 10 && result.color >= 5) {
+			XSetForeground(display, DefaultGC(display, s), orange.pixel);
+		} else if (result.color < 10 && result.color > 0) {
+			XSetForeground(display, DefaultGC(display, s), blue.pixel);
 		} else {
-			XSetForeground(display, DefaultGC(display, s), WhitePixel(display, s));	
+			XSetForeground(display, DefaultGC(display, s), BlackPixel(display, s));
 		}
         XFillRectangle(display, window, DefaultGC(display, s), result.x, result.y, 1, 1);
 
-        /* exit on key press */
         if (event.type == KeyPress)
             break;
 
-		pthread_mutex_unlock(&buffer_mutex);
+		pthread_mutex_unlock(&buffer_mutex); //Libera o lock
 	}
  
-    /* close connection to server */
     XCloseDisplay(display);
 }
